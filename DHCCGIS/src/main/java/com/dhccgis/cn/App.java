dd9bb04f268bbc9e361bbb5333e810884717d78a
xingjian <xingjian@9a834573-2090-804c-9287-1a834bdb27e7>
2010-12-27 02:10:06 +0000

    dhccgis
    git-svn-id: svn://10.10.191.1:3690/DHCCGIS/trunk@47 9a834573-2090-804c-9287-1a834bdb27e7

---
+++ DHCCGIS/src/main/java/com/dhccgis/cn/App.java
@@ -0,0 +1,13 @@
+package com.dhccgis.cn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
